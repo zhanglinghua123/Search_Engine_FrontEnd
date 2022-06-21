b05159b0646b0ca106e0a4cbe506ced43e0bf2e6
zhanglinghua123 <1441749449@qq.com>
2022-06-21 18:04:42 +0800

    init

---
+++ src/page/fontpage/index.tsx
@@ -0,0 +1,10 @@
+import './style/index.less';
+export const FontPage = () => {
+    const prefixCls = 'fontpage';
+
+    return (
+        <div>
+            <div className={`${prefixCls}-head`}></div>
+        </div>
+    );
+};
