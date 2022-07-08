015cfec7206a6ca18a215a136fb87cd7a65caa23
zhanglinghua123 <1441749449@qq.com>
2022-07-08 16:05:06 +0800

    修正page文件夹名称

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { FontPage } from './page/Fontpage/index';
+import { FontPage } from './page/Homepage/index';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { QueryResult } from './page/QueryResult';
 import { Player } from './page/Player';
